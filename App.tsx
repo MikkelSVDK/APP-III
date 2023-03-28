0ed5b5dc72f6db8e4399c45a2e3b49946e2b3c39
Mikkel Vestergård <mikkel@mikkelsv.dk>
2023-03-28 10:10:09 +0200

    added home screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+import { unlockAsync } from 'expo-screen-orientation'
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Home from './screens/Home'
+import Qr from './screens/Qr'
+import Location from './screens/Location'
+import Sensors from './screens/Sensors'
+import AccelerometerScreen from './screens/Sensors/Accelerometer'
+import BarometerScreen from './screens/Sensors/Barometer'
+import GyroscopeScreen from './screens/Sensors/Gyroscope'
+import LightSensorScreen from './screens/Sensors/LightSensor'
+import MagnetometerScreen from './screens/Sensors/Magnetometer'
+import HapticsScreen from './screens/Sensors/Haptics'
+import CameraScreen from './screens/Camera'
+
+preventAutoHideAsync()
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
+  unlockAsync()
+  hideAsync()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Qr" component={Qr} />
+        <Stack.Screen name="Location" component={Location} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen name="Sensors" component={Sensors} />
+        
+        <Stack.Screen name="Accelerometer" component={AccelerometerScreen} />
+        <Stack.Screen name="Barometer" component={BarometerScreen} />
+        <Stack.Screen name="Gyroscope" component={GyroscopeScreen} />
+        <Stack.Screen name="LightSensor" component={LightSensorScreen} />
+        <Stack.Screen name="Magnetometer" component={MagnetometerScreen} />
+        <Stack.Screen name="Haptics" component={HapticsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Home.tsx
@@ -0,0 +1,32 @@
+import { Button, StyleSheet, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+export default function Home({ navigation }: NativeStackScreenProps<any>) {
+  return (
+    <View style={styles.container}>
+      <View style={{marginBottom: 10}}>
+        <Button title="Go to Camera" onPress={() => navigation.navigate('Camera')} />
+      </View>
+      <View style={{marginBottom: 10}}>
+        <Button title="Go to Location" onPress={() => navigation.navigate('Location')} />
+      </View>
+      <View style={{marginBottom: 10}}>
+        <Button title="Go to QR scanner" onPress={() => navigation.navigate('Qr')} />
+      </View>
+      <Button title="Go to Sensors" onPress={() => navigation.navigate('Sensors')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
